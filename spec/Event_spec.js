fc687323738e9ac0c42e889f8e7e056bc3535547
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-24 14:06:20 +0000

    updated spec file

--- spec/Event_spec.js
+++ spec/Event_spec.js
@@ -1,20 +1,15 @@
 describe('Event', function () {
   describe('#create', function() {
     var an_event = new Event();
-    var diary = new EventDiary();
     an_event.createEvent('Workshop', '2020/01/01', '10:00');
-    diary.saveEvent(an_event);
-    can('add an event to the Event Diary', function () {
-      expect(diary._eventList.length).toEq(1)
+    can('know an event content', function () {
+      expect(an_event.getContent()).toEq('Workshop')
     });
-    can('read its content', function () {
-      expect(an_event._content).toEq('Workshop')
+    can('know an event date', function () {
+      expect(an_event.getDate()).toEq('2020/01/01')
     });
-    can('read its date', function () {
-      expect(an_event._date).toEq('2020/01/01')
-    });
-    can('read its time', function () {
-      expect(an_event._time).toEq('10:00')
+    can('know an event time', function () {
+      expect(an_event.getTime()).toEq('10:00')
     });
 
   });
